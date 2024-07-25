e9dbc885e294db898886cbf79a4d7e8a917bbe4e
Royce Joe <roylory@gmail.com>
2024-07-25 15:28:04 -0700

    add log4day in the list

--- src/Home/index.tsx
+++ src/Home/index.tsx
@@ -7,15 +7,24 @@ function Page() {
     <div className="container m-auto p-4 h-lvh flex flex-col justify-start">
       <div className="m-auto flex-auto flex flex-col justify-start">
         <div className="flex-auto flex flex-col">
-          <h1 className="text-2xl font-semibold my-4 mb-8 text-center">
+          <h1 className="text-2xl font-semibold mt-8 mb-16 text-center">
             <span className="text-pink-900">Splen</span>Works
-            <span className="font-normal">&apos; Products</span>
+            <span className="font-normal">
+              &apos; <i>Products</i>
+            </span>
           </h1>
           <div className="flex-auto flex items-first">
             <ul className="[&>li]:px-6 [&>li]:py-4">
               <li>
-                <HyperLink url="https://txtpad.io" text="txtpad.io" newTab />
-                <p>Free online text editor. Write and save your notes online</p>
+                <HyperLink
+                  url="https://txtpad.io"
+                  text="txtpad.io"
+                  newTab
+                  className=""
+                />
+                <p>
+                  Free online text editor. Write and save your notes online.
+                </p>
               </li>
               <li>
                 <HyperLink
@@ -23,7 +32,7 @@ function Page() {
                   text="SplenView"
                   newTab
                 />
-                <p>A simple image viewer like HoneyView but in the browser</p>
+                <p>A simple image viewer like HoneyView but in the browser.</p>
               </li>
               <li>
                 <HyperLink
@@ -31,13 +40,19 @@ function Page() {
                   text="SplenPlayer"
                   newTab
                 />
-                <p>A simple video/music player that can handle subtitles too</p>
+                <p>
+                  A simple video/music player that can handle subtitles too.
+                </p>
+              </li>
+              <li>
+                <HyperLink url="https://log4day.com" text="log4day" disabled />
+                <p>Diary app in development.</p>
               </li>
             </ul>
           </div>
         </div>
         <footer>
-          <p className="text-sm left-0 right-0 bottom-0 py-4 text-center">
+          <p className="text-sm left-0 right-0 bottom-0 py-8 text-center">
             &copy; {currentYear} SplenWorks Inc. All rights reserved.
           </p>
         </footer>
--- src/shared/HyperLink.tsx
+++ src/shared/HyperLink.tsx
@@ -1,17 +1,35 @@
 import React from "react"
+import { twMerge } from "tailwind-merge"
 
 interface HyperLinkProps {
   url: string
   text: string
   newTab?: boolean
+  disabled?: boolean
+  className?: string
 }
 
-const HyperLink: React.FC<HyperLinkProps> = ({ url, text, newTab }) => {
+const HyperLink: React.FC<HyperLinkProps> = ({
+  url,
+  text,
+  newTab,
+  disabled,
+  className,
+}) => {
   return (
     <a
       href={url}
       target={newTab ? "_blank" : "_self"}
-      className="text-pink-900 hover:underline hover:text-pink-700 focus-within:outline-pink-700"
+      className={twMerge(
+        "text-pink-800 hover:underline hover:text-pink-600 focus-within:outline-pink-600",
+        disabled && "cursor-default hover:no-underline",
+        className,
+      )}
+      onClick={(e) => {
+        if (disabled) {
+          e.preventDefault()
+        }
+      }}
     >
       {text}
     </a>
